2daa142d73721365558a37fd741c028ef53d7b3c
mcharyta <103214073+mcharyta@users.noreply.github.com>
2022-04-08 00:14:54 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,23 @@
+class Hello
+{
+
+public static void main(String[] args)
+{
+
+System.out.println("Welcome to JAVA world");
+
+	int a,b,c;
+
+	a=100;
+	b=200;
+	c=a+b;
+
+System.out.println("Addition of two numbers a and b is :"  + c);
+
+}
+}
+
+
+
+
+
